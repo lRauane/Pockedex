f98399324869b8fb1f76584994bfcc545ca55d4f
Rauane Lima <rauanelima7@gmail.com>
2022-11-03 19:31:14 -0300

    _utilizando Map para diminuir código

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,15 +18,9 @@ function convertPokemonToLi(pokemon) {
   `;
 }
 
-const pokemonList = document.getElementById("pokemon-list")
+const pokemonList = document.getElementById("pokemon-list");
 
-fetch(url)
-  .then((response) => response.json())
-  .then((jsonBody) => jsonBody.results)
-  .then((pokemons) => {
-    for (let i = 0; i < pokemons.length; i++) {
-      const pokemon = pokemons[i];
-      pokemonList.innerHTML += convertPokemonToLi(pokemon)
-    }
-  })
-  .catch((error) => console.log(error));
+// Manipulando listas
+pokeApi.getPokemons().then((pokemons) => {
+  pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+});
