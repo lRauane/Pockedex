2bea294ea86615593c79d1b0602b4ebc9f8df46e
Rauane Lima <rauanelima7@gmail.com>
2022-11-05 14:46:15 -0300

    _ estilizando header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,25 @@
 const pokemonList = document.getElementById("pokemon-list");
 const LoadMoreButton = document.getElementById("loadMoreButton")
+
+
 const limit = 5
 let offset = 0
 const maxRecords = 151
-
 
 function loadPokemonItems(offset, limit){
 
   pokeApi.getPokemons(offset, limit).then((pokemons) => {
     const newHtml = pokemons.map((pokemon) => `
-    <li class="pokemon ${pokemon.type}">
-        <span class="pokemon-number">#${pokemon.number}</span>
-        <span class="pokemon-name">${pokemon.name}</span>
-        <div class="details">
-          <ol class="types">
-            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join(' ')}
-          </ol>
-          <img src="${pokemon.photo}" alt="${pokemon.name}" class="detail-image">
-        </div>
-    </li>
+      <li class="pokemon ${pokemon.type}">
+          <span class="pokemon-number">#${pokemon.number}</span>
+          <span class="pokemon-name">${pokemon.name}</span>
+          <div class="details">
+            <ol class="types">
+              ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join(' ')}
+            </ol>
+            <img src="${pokemon.photo}" alt="${pokemon.name}" class="detail-image">
+          </div>
+      </li>
       `).join('')
 
     pokemonList.innerHTML += newHtml
